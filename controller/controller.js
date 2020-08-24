da1da6bb5608b0cae439ca184022aede25b81b7d
Sandy Smith <toosandra@gmail.com>
2020-08-24 10:22:30 -0400

    resolving committing issue with git

---
+++ controller/controller.js
@@ -0,0 +1,6 @@
+var express = require("express");
+var router = express.Router();
+var path = require("path");
+
+var request = require("request");
+var cheerio = require("cheerio");
---
+++ server.js
@@ -0,0 +1,48 @@
+//dependencies
+var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
+var logger = require("morgan");
+
+//initialize Express app
+var express = require("express");
+var app = express();
+
+app.use(logger("dev"));
+app.use(
+  bodyParser.urlencoded({
+    extended: false
+  })
+);
+
+app.use(express.static(process.cwd() + "/public"));
+//Require set up handlebars
+var exphbs = require("express-handlebars");
+app.engine(
+  "handlebars",
+  exphbs({
+    defaultLayout: "main"
+  })
+);
+app.set("view engine", "handlebars");
+
+//connecting to MongoDB
+//mongoose.connect("mongodb://localhost/scraped_news");
+const MONGODB_URI =
+  process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
+
+var db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", function() {
+  console.log("Connected to Mongoose!");
+});
+
+var routes = require("./controller/controller.js");
+app.use("/", routes);
+
+
+//Create localhost port
+var port = process.env.PORT || 3000;
+app.listen(port, function() {
+  console.log("Listening on PORT " + port);
+});
